89271675060806a1e87936d6ff538784d478bf5a
JinYuSha0 <a1009943858@gmail.com>
2023-12-15 20:32:35 +0800

    update

--- nestjs-remix/client/helper.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function noop(): any;
-export declare function deferred<T>(): {
-    promise: Promise<T>;
-    resolve: (value: T | PromiseLike<T>) => void;
-    reject: (reason?: any) => void;
-};
